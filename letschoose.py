c302adebc4982c8fc4af7483c83d60bc4000ac93
Bentaib Mohammed Yazid <78019553+mohammedyazid@users.noreply.github.com>
2022-04-09 01:32:56 +0100

    Some bugs fixed

--- letschoose.py
+++ letschoose.py
@@ -4,7 +4,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 updater = Updater('5192566556:AAGkSw2h6tK7YXn4DsKA1R5qtSAh6CpVB7k', use_context=True)
 
-Answers=[]*10
+Answers=[]
 class questioncounter:
       i=0
 
@@ -13,41 +13,40 @@ print("Bot Started...")
 def start(update, context):
   update.message.reply_text(main_welcome(),reply_markup=main_menu_keyboard())
   
-
 def main_menu(update,context):
   query = update.callback_query
   query.answer()
-  query.edit_message_text(
-                        text=main_welcome(),
-                        reply_markup=main_menu_keyboard())
+  query.edit_message_text(text=main_welcome(),reply_markup=main_menu_keyboard())
 
 def first_menu(update,context):
+  Answers.clear()
+  questioncounter.i=0
   query = update.callback_query
   query.answer()
-  query.edit_message_text(
-                        text=first_question(),
-                        reply_markup=first_menu_keyboard())
+  query.edit_message_text(text=first_question(),reply_markup=first_menu_keyboard())
 
-def about_bot(update,context):
+def about(update,context):
   query = update.callback_query
   query.answer()
-  query.edit_message_text(text=about())
+  query.edit_message_text(text=abouttext(),reply_markup=back())
 
-# # and so on for every callback_data option
-# def first_submenu(bot, update):
-#   pass
-
-# def second_submenu(bot, update):
-#   pass
-
-# def second_submenu(bot, update):
-#   pass
-
+def nvm(update,context):
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(text=nvmtext(),reply_markup=Nothing())
 ############################ Keyboards #########################################
 def main_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Start the chat', callback_data='start')],
-              [InlineKeyboardButton('About the bot', callback_data='about')],
-              [InlineKeyboardButton('Nevermind', callback_data='nvm')]]
+  keyboard = [[InlineKeyboardButton('Start the chat🏁', callback_data='start')],
+              [InlineKeyboardButton('About the bot🤷', callback_data='about')],
+              [InlineKeyboardButton('Nevermind❌', callback_data='Nevermind')]]
+  return InlineKeyboardMarkup(keyboard)
+
+def back():
+  keyboard = [[InlineKeyboardButton('Back ⬅️', callback_data='Main')],]
+  return InlineKeyboardMarkup(keyboard)
+
+def Nothing():
+  keyboard = [[InlineKeyboardButton('✌🏼', callback_data='Main')],]
   return InlineKeyboardMarkup(keyboard)
 
 def first_menu_keyboard():
@@ -57,7 +56,7 @@ def first_menu_keyboard():
               [InlineKeyboardButton('Literature and Philosophy', callback_data='Literature and Philosophy')],
               [InlineKeyboardButton('Foreign languages', callback_data='Foreign languages')],
               [InlineKeyboardButton('Management and Economy', callback_data='Management and Economy')],
-              [InlineKeyboardButton('Main menu', callback_data='main')]]
+              [InlineKeyboardButton('Main menu⬅️', callback_data='Main')]]
   return InlineKeyboardMarkup(keyboard)
 
 def second_menu_keyboard():
@@ -71,11 +70,13 @@ def second_menu_keyboard():
               [InlineKeyboardButton('17-18', callback_data='17-18')],
               [InlineKeyboardButton('18-19', callback_data='18-19')],
               [InlineKeyboardButton('19+', callback_data='19+')],
-              [InlineKeyboardButton('Main menu', callback_data='main')]]
+              [InlineKeyboardButton('Main menu⬅️', callback_data='Main')]]
   return InlineKeyboardMarkup(keyboard)
 
 def third_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('you good', callback_data='good')],
+  keyboard = [
+    [InlineKeyboardButton('you good', callback_data='good')],
+    [InlineKeyboardButton('Main menu⬅️', callback_data='Main')]
               ]
   return InlineKeyboardMarkup(keyboard)
 
@@ -84,7 +85,7 @@ def third_menu_keyboard():
 def save(update,context):
   print(questioncounter.i)
   query = update.callback_query
-  context.bot.sendMessage(chat_id=query.message.chat_id,text="Your answer was {}.".format(query.data))
+  context.bot.sendMessage(chat_id=query.message.chat_id,text="🤖:Your answer was {}.".format(query.data))
   Answers.append(str(query.data))
   questioncounter.i+=1
   print(Answers)
@@ -92,30 +93,40 @@ def save(update,context):
 
 ################################### Menu's######################################
 def redirect(query,i):
+  if(i==0):
+    query.edit_message_text(text=first_question(),reply_markup=first_menu_keyboard())
   if(i==1):
     query.edit_message_text(text=second_question(),reply_markup=second_menu_keyboard())
   if(i==2):
-    query.edit_message_text(text=second_question(),reply_markup=third_menu_keyboard())
+    query.edit_message_text(text=third_question(),reply_markup=third_menu_keyboard())
 
 ############################# Messages #########################################
 def main_welcome():
   return 'Hi how can i help you?'
 
-def about():
-  return "I'm a Bot created by three Computer Science students i'll tell you more about myself when i'm ready..."
+def abouttext():
+  return "I'm a Bot created by three Computer Science students. My job is to help you decide your future, i will give you a choice that might fits you but always remember 'Choose what you like not what others like'"
+
+def nvmtext():
+  return 'Alright you can reach me anytime using " /start " have a great day 😄 '
 
 def first_question():
   return 'What is your major in baccalaureate?'
 
 def second_question():
-  return 'How much was your bac score?'
+  return 'What was your score in BAC?'
+
+def third_question():
+  return 'What was your score in BAC?'
 
 ############################# Handlers #########################################
 updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('about', about))
 
-updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
 updater.dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='start'))
-updater.dispatcher.add_handler(CallbackQueryHandler(about_bot, pattern='about'))
+updater.dispatcher.add_handler(CallbackQueryHandler(about, pattern='about'))
+updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='Main'))
+updater.dispatcher.add_handler(CallbackQueryHandler(nvm, pattern='Nevermind'))
 
 updater.dispatcher.add_handler(CallbackQueryHandler(save,pattern='Science'))
 updater.dispatcher.add_handler(CallbackQueryHandler(save,pattern='Math'))
