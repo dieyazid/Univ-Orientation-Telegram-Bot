07ed2bb6a413ade126613c43ff44ef93d4a17f4c
Ya21d <78019553+mohammedyazid@users.noreply.github.com>
2022-10-24 22:31:43 +0100

    Update letschoose.py

--- letschoose.py
+++ letschoose.py
@@ -5,7 +5,7 @@ from datetime import datetime
 #Hello
 from expert import getdata
 
-updater = Updater('5192566556:AAGkSw2h6tK7YXn4DsKA1R5qtSAh6CpVB7k', use_context=True)
+updater = Updater('Token in here', use_context=True)
 
 Answers=[]
 Users=[]
